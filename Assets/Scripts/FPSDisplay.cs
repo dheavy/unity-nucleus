8f94b8b08614f4e0c0e225f0ef006613ab579c8d
Davy Peter Braun <davy.braun@callr.com>
2017-06-24 11:51:44 +0200

    Refactor, measure and display highest, average and lowest FPS

--- Assets/Scripts/FPSDisplay.cs
+++ Assets/Scripts/FPSDisplay.cs
@@ -10,8 +10,6 @@ public class FPSDisplay : MonoBehaviour {
     // in a buffer, using an index to set where the put
     // data on the next frame.
     FPSCounter fpsCounter;
-    int fpsBufferIndex;
-    int[] fpsBuffer;
 
     static string[] stringsFrom00To99 = {
 		"00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
@@ -26,61 +24,15 @@ public class FPSDisplay : MonoBehaviour {
 		"90", "91", "92", "93", "94", "95", "96", "97", "98", "99"
 	};
 
-    public int HighestFPS { get; private set; }
-    public int LowestFPS { get; private set; }
-
-    void InitializeBuffer () 
-    {
-        if (frameRange <= 0) {
-            frameRange = 1;
-        }
-        fpsBuffer = new int[frameRange];
-        fpsBufferIndex = 0;
-    }
-
     void Awake () 
     {
         fpsCounter = GetComponent<FPSCounter>();
     }
 
-    void Update () 
+    void Update ()
     {
-        if (fpsBuffer == null || fpsBuffer.Length != frameRange) {
-            InitializeBuffer();
-        }
-        UpdateBuffer();
-        CalculateFPS();
-    }
-
-    void UpdateBuffer () 
-    {
-        fpsBuffer[fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
-        if (fpsBufferIndex >= frameRange) {
-            fpsBufferIndex = 0;
-        }
-    }
-
-    void CalculateFPS ()
-    {
-        int sum = 0;
-        int highest = 0;
-        int lowest = int.MaxValue;
-
-        for (int i = 0; i < frameRange; i++) {
-            int fps = fpsBuffer[i];
-            sum += fps;
-
-            if (fps > highest) {
-                highest = fps;
-            }
-
-            if (fps < lowest) {
-                lowest = fps;
-            }
-        }
-        
-        AverageFPS = sum / frameRange;
-        HighestFPS = highest;
-        LowestFPS = lowest;
+        highestFPSLabel.text = stringsFrom00To99[Mathf.Clamp(fpsCounter.HighestFPS, 0, 99)];
+        averageFPSLabel.text = stringsFrom00To99[Mathf.Clamp(fpsCounter.AverageFPS, 0, 99)];
+        lowestFPSLabel.text = stringsFrom00To99[Mathf.Clamp(fpsCounter.LowestFPS, 0, 99)];
     }
 }
